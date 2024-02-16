2e126ab2a314e564ea92166ac1a6c35656cc7b36
Noor Ul Islam <noorulislam770@gmail.com>
2024-02-16 12:10:08 +0500

    Implement Searcing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,15 @@ import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
-const Navbar = () => {
+
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+const Navbar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
       <Image height="30px" src={logo} />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
